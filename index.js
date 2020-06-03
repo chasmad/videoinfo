ae5329d5dbf97f4569f9659fe525f1f876d305c9
Daniel Beck <dlc.beck@gmail.com>
2020-06-03 14:49:39 -0700

    add ipc integration

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const electron = require('electron');
 
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, ipcMain } = electron;
 
 app.on('ready', () => {
    const mainWindow = new BrowserWindow({
@@ -9,4 +9,8 @@ app.on('ready', () => {
       }
    });
    mainWindow.loadURL(`file://${__dirname}/index.html`);
+});
+
+ipcMain.on('video:submit', () => {
+
 });
